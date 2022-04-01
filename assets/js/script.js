02e7e22ebc17c745662f3882edad6d6003df8f95
cmwschroeder <cmwschroeder@gmail.com>
2022-04-01 15:15:44 -0700

    added comments to algorithm for better understanding

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,15 +25,29 @@ submitEl.addEventListener("click", function (event) {
   event.preventDefault();
   var inputText = inputEl.value;
   inputEl.value = "";
+
+  // get the current answer being shown and make it into an array so we can easily compare it
+  // to the word that we are trying to guess. This will still have _'s in spots with letters we
+  // haven't guessed
   var currentAns = answerEl.textContent.split("").filter(letter => letter != " ");
+
+  // checks if the answer word has the letter selected
   if (selected.includes(inputText)) {
+    //if it does we need to rewrite it on the page with blanks filled in
+    // so we first make the current guesses empty
     answerEl.textContent = "";
 
+    // we then get the new text with the letter guessed filled into the answer field
+    // in order to display that text. We basically are just filing in the _'s with
+    // the letter that was guessed correctly
     for(var i = 0; i < selected.length; i++) {
       if(selected[i] == inputText) {
         currentAns[i] = inputText;
       }
     }
+
+    // Now we want to display the word with the letter we guessed filled in so we
+    // rewrite the entire word
     for(var i = 0; i < currentAns.length; i++) {
       answerEl.textContent += currentAns[i] + " ";
     }
